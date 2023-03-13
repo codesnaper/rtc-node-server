5178633a0e51ef5954d090977ff03abf230025e1
Shubham Khandelwal <shubham.khandelwal019@gmail.com>
2023-03-13 21:26:26 +0100

    Remove build

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./service/ws/server");
-var util_1 = require("./util");
-var logger = new util_1.Util().log({ application: 'main-server' });
-new server_1.AppWSServer().createServer();
-logger.info('WebSocket server initialized initialized');
